2bb91984790bc5716473ce64288985e1cbcd4aeb
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2023-07-04 23:59:06 +0200

    Refactor de los nombres de las clases AnimePeliculas y AnimeSeries. Añadimos JavaDoc que faltaba.

--- Models/Anime.java
+++ Models/Anime.java
@@ -3,10 +3,24 @@ package Models;
 import ENUM.Genero;
 import ENUM.Pegi;
 
+/**
+ * Creación de la clase Anime, que hereda de ContenidoAudioVisual.
+ */
 public class Anime extends ContenidoAudioVisual {
     protected int numeroCapitulos;
     protected boolean finalizado;
 
+    /**
+     * Método constructor de la clase Anime.
+     *
+     * @param nombre
+     * @param genero
+     * @param anyoLanzamiento
+     * @param duracionMinutos
+     * @param director
+     * @param pegi
+     * @param puntuacion
+     */
     public Anime(String nombre, Genero genero, int anyoLanzamiento, String duracionMinutos, String director, Pegi pegi, float puntuacion) {
         super(nombre, genero, anyoLanzamiento, duracionMinutos, director, pegi, puntuacion);
     }
