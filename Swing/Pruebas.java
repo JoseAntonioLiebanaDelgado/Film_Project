119e36b9c783e54a8261c0e8d302ee37a971aed3
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2023-07-18 18:14:07 +0200

    Se ha creado una clase para hacer pruebas y se han insertado 8 containers con una label en cada uno en el panel central de la mitad izquierda del frame padre.

--- Swing/Pruebas.java
+++ Swing/Pruebas.java
@@ -12,10 +12,31 @@ public class Pruebas {
         JFrame framePadre = new JFrame();
 
         JPanel panelPrincipalIzquierdo = new JPanel();
+
         JPanel containerPanelSuperiorIzquierdo = new JPanel();
         JPanel panelSuperiorIzquierdo = new JPanel();
+
         JPanel containerPanelCentralIzquierdo = new JPanel();
-        JPanel panelCentralIzquierdo = new JPanel();
+        GridLayout disposicionPanelCentralIzquierdoGridLayout = new GridLayout(4, 2);
+        JPanel panelCentralIzquierdo = new JPanel(disposicionPanelCentralIzquierdoGridLayout);
+
+        JPanel containerLabel1de8 = new JPanel();
+        JLabel label1de8 = new JLabel("Frodo");
+        JPanel containerLabel2de8 = new JPanel();
+        JLabel label2de8 = new JLabel("Sam");
+        JPanel containerLabel3de8 = new JPanel();
+        JLabel label3de8 = new JLabel("Pippin");
+        JPanel containerLabel4de8 = new JPanel();
+        JLabel label4de8 = new JLabel("Merry");
+        JPanel containerLabel5de8 = new JPanel();
+        JLabel label5de8 = new JLabel("Gandalf");
+        JPanel containerLabel6de8 = new JPanel();
+        JLabel label6de8 = new JLabel("Aragorn");
+        JPanel containerLabel7de8 = new JPanel();
+        JLabel label7de8 = new JLabel("Legolas");
+        JPanel containerLabel8de8 = new JPanel();
+        JLabel label8de8 = new JLabel("Gimli");
+
         JButton botonInferiorIzquierdo = new JButton("Añadir");
 
         JPanel panelPrincipalDerecho = new JPanel();
@@ -36,6 +57,23 @@ public class Pruebas {
         containerPanelCentralIzquierdo.setOpaque(false);
         panelCentralIzquierdo.setBackground(Color.lightGray);
 
+        containerLabel1de8.setBackground(Color.GREEN);
+        label1de8.setBackground(Color.MAGENTA);
+        containerLabel2de8.setBackground(Color.blue);
+        label2de8.setBackground(Color.MAGENTA);
+        containerLabel3de8.setBackground(Color.yellow);
+        label3de8.setBackground(Color.MAGENTA);
+        containerLabel4de8.setBackground(Color.white);
+        label4de8.setBackground(Color.MAGENTA);
+        containerLabel5de8.setBackground(Color.red);
+        label5de8.setBackground(Color.MAGENTA);
+        containerLabel6de8.setBackground(Color.cyan);
+        label6de8.setBackground(Color.MAGENTA);
+        containerLabel7de8.setBackground(Color.magenta);
+        label7de8.setBackground(Color.MAGENTA);
+        containerLabel8de8.setBackground(Color.lightGray);
+        label8de8.setBackground(Color.MAGENTA);
+
         botonInferiorIzquierdo.setBackground(Color.darkGray);
 
         panelPrincipalDerecho.setBackground(Color.blue);
@@ -53,6 +91,24 @@ public class Pruebas {
 
         containerPanelSuperiorIzquierdo.add(panelSuperiorIzquierdo);
         containerPanelCentralIzquierdo.add(panelCentralIzquierdo);
+
+        containerLabel1de8.add(label1de8);
+        containerLabel2de8.add(label2de8);
+        containerLabel3de8.add(label3de8);
+        containerLabel4de8.add(label4de8);
+        containerLabel5de8.add(label5de8);
+        containerLabel6de8.add(label6de8);
+        containerLabel7de8.add(label7de8);
+        containerLabel8de8.add(label8de8);
+
+        panelCentralIzquierdo.add(containerLabel1de8);
+        panelCentralIzquierdo.add(containerLabel2de8);
+        panelCentralIzquierdo.add(containerLabel3de8);
+        panelCentralIzquierdo.add(containerLabel4de8);
+        panelCentralIzquierdo.add(containerLabel5de8);
+        panelCentralIzquierdo.add(containerLabel6de8);
+        panelCentralIzquierdo.add(containerLabel7de8);
+        panelCentralIzquierdo.add(containerLabel8de8);
 
         panelPrincipalIzquierdo.add(botonInferiorIzquierdo);
 
